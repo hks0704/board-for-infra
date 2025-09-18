af107b00f148cca083ab40dc65261595f871faf1
hks0704 <cldy7293@gmail.com>
2025-09-19 02:08:23 +0900

    CORS 설정 추가

--- backend/src/main/java/com/toyproject/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/toyproject/backend/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:5173", "http://localhost:8080")
+			.allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://localhost:8080")
 			.allowedMethods(
 				HttpMethod.GET.name(),
 				HttpMethod.POST.name(),
